a48cf3f8b1645468ff03586d23ea5d254295f67b
AdrianCalo <110639622+AdrianCalo@users.noreply.github.com>
2023-05-29 18:43:46 -0300

    Delete Entregables directory

--- Entregables/Integrador Programacion Orientada a Objetos/Trabajo Integrador Grupal - Registro de Colegio/interfacePersonas.ts
+++
@@ -1,8 +0,0 @@
-//Creacion de la interfas Personas.
-//"Firma de Contrato" por parte de las clases Alumnos y Profesores.
-export default interface Personas {
-    nombre: string;
-    apellido: string;
-    dni: number;
-    fechaNacimiento: string;    
-}
--- Entregables/Integrador Programacion Orientada a Objetos/integrador 2da version/Materias.ts
+++
@@ -1,9 +0,0 @@
-export class Materias {
-    private nombre:string;
-    private nota: number;
-
-    constructor(nombre:string,nota:number){
-        this.nombre=nombre;
-        this.nota=nota;
-    }
-}
